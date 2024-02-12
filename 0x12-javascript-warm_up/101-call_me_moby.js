eb25f71e7b146eb1446fb4b319046645af154b5a
zikotan <tananzakaria630@gmail.com>
2024-02-13 00:30:14 +0100

    response

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-
-exports.callMeMoby = function (x, theFunction) {
-  let i;
-  for (i = 0; i < x; i++) theFunction();
-};
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-
-const myString = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-let i;
-
-for (i = 0; i < 3; i++) {
-  console.log(myString[i]);
-}
