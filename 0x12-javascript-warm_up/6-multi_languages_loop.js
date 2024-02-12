7df3349f5c1be283ab3c2934a8e5ec6669438be9
zikotan <tananzakaria630@gmail.com>
2024-02-13 00:05:49 +0100

    response

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const myString = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i;
+
+for (i = 0; i < 3; i++) {
+  console.log(myString[i]);
+}
---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const size = Math.floor(Number(process.argv[2]));
+let r, c, row;
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (r = 0; r < size; r++) {
+    row = '';
+    for (c = 0; c < size; c++) 
+      row += 'X';
+    console.log(row);
+  }
+}
