cecc89f9b60296493a124751019ff9b4709fb797
zikotan <tananzakaria630@gmail.com>
2024-02-16 18:44:21 +0100

    response

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -9,8 +9,9 @@ if __name__ == '__main__':
     import MySQLdb
     import sys
 
-    myDB = MySQLdb.connect(host='localhost', port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    myDB = MySQLdb.connect(
+        host='localhost', port=3306, user=sys.argv[1],
+        passwd=sys.argv[2], db=sys.argv[3])
 
     myCursor = myDB.cursor()
     myCursor.execute("SELECT * FROM states WHERE name=%s\
