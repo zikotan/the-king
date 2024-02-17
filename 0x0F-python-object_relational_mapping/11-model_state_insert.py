54f628ec297f70ac752ef1f047ad9975a39e05db
zikotan <tananzakaria630@gmail.com>
2024-02-17 20:25:00 +0100

    response

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""adding the “Louisiana” State object to hbtn_0e_6_usa"""
+
+if __name__ == "__main__":
+
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    from sqlalchemy.schema import Table
+
+    myEng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(myEng)
+
+    mySess = Session(myEng)
+    myNew = State(name='Louisiana')
+    mySess.add(myNew)
+    myState = mySess.query(State).filter(State.name == 'Louisiana').first()
+    mySess.commit()
+    print("{}".format(myState.id))
+    mySess.close()
