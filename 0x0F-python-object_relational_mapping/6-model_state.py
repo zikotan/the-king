bcf03ef83bc7ea525c99db6c27790c3f46948ac6
zikotan <tananzakaria630@gmail.com>
2024-02-17 16:49:33 +0100

    response

---
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Starting link class to table in database"""
+import sys
+from model_state import Base
+
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    myEng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(myEng)
---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""listing all the hbtn_0e_6_usa State objects"""
+
+if __name__ == "__main__":
+
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+
+    myEng = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
+        sys.argv[2],sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(myEng)
+
+    mySession = Session(myEng)
+    for s in mySession.query(State).order_by(State.id).all():
+        print("{}: {}".format(s.id, s.name))
+    mySession.close()
